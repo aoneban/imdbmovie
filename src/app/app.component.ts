272e5d6c4b282bf6b0af32e69033cc2ab806ffca
aoneban <torgregion1@gmail.com>
2024-12-13 18:52:59 +0100

    feat: add husky to the project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,15 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+
   imports: [RouterOutlet],
+
   template: `
-    <h1>Welcome to {{title}}!</h1>
+    <h1>Welcome to {{ title }}!</h1>
 
     <router-outlet />
   `,
+
   styles: [],
 })
 export class AppComponent {
